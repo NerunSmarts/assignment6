255f3c05875427f6ef01b9927b3a9d3a35c72b43
NerunSmarts <neruntoneron@gmail.com>
2024-04-29 19:09:40 -0500

    Grabbing image from server

--- src/main/java/com/nerunsmarts/PrimaryController.java
+++ src/main/java/com/nerunsmarts/PrimaryController.java
@@ -1,11 +1,23 @@
 package com.nerunsmarts;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 
 public class PrimaryController {
 
     @FXML
+    public void initialize() {
+        Image logo = null;
+        try {
+            logo = new Image("https://images.squarespace-cdn.com/content/v1/5d56a346839a3a000100fcda/1565961231827-2I2YZCLMEQQX6S69V8PN/pizzalandlogo.png");
+        } catch (Exception e) {
+            System.out.println("Error! Image inaccesible!");
+        }
+        System.out.println("PrimaryController initialized");
+    }
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
