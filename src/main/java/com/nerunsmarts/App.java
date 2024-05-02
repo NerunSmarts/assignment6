4d843a1f3ffcb9c03f29423470bfb9c2ed24563b
NerunSmarts <neruntoneron@gmail.com>
2024-05-02 10:31:43 -0500

    don't remember what I changed, probably a layout or comment thing

--- src/main/java/com/nerunsmarts/App.java
+++ src/main/java/com/nerunsmarts/App.java
@@ -31,6 +31,7 @@ import javafx.stage.Stage;
 // also, this is the probably the hardest code to read that i've ever written. I am not sorry.
 // also also this is just a shitty version of SwitfUI. Please let me use that next time lol cause then I can build for my iPhone.
 // also also also i did this at 11:20 on Monday night and I guarantee I could get better formatting on literally any other graphics package. Or if I was allowed to have multiple screens and use my own layout. I used to do web design.
+// last comment i swear but I fixed some formatting stuff so above might not apply as much anymore.
 
 public class App extends Application {
 
